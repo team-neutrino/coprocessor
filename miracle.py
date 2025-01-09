eda45432e0c6668f094b820acb58f714b43b1d94
Team Neutrino <neutrino3928controls@outlook.com>
2025-01-08 18:44:37 -0600

    works

--- miracle.py
+++ miracle.py
@@ -13,12 +13,11 @@ import sys
 import time
 from networktables import NetworkTables
 
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
+
 # To see messages from networktables, you must setup logging
 import logging
-
-'''import importlib
-importlib.import_module("/home/pi/Raspberry_Pi_Installer_Scripts/rpi_rgb_led_matrix/bindings/python/samples/gif_viewer")
-'''
 
 import sys
 sys.path.insert(0, '/home/pi/Raspberry_Pi_Installer_Scripts/rpi_rgb_led_matrix/bindings/python/samples')
@@ -41,17 +40,33 @@ matrix = None
 options = None
 
 
-
 def valueChanged(table, key, value, isNew):
     print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
-    image_viewer.image_player("/home/pi/Downloads/trino2.jpg", image, options, matrix)
+    image_player("/home/pi/Downloads/trino2.jpg", image, options, matrix)
     print(matrix)
-
-
 
 
 def connectionListener(connected, info):
     print(info, "; Connected=%s" % connected)
+
+def image_player(image_name):
+    image = Image.open(image_name)
+    `
+    # Configuration for the matrix
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 64
+    options.chain_length = 1
+    options.parallel = 1
+    options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+    options.gpio_slowdown = 2
+    options.brightness = 50
+
+    matrix = RGBMatrix(options = options)
+
+    image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+    matrix.SetImage(image.convert('RGB'))
+    time.sleep(100)
 
 
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
@@ -62,5 +77,4 @@ bb = intake.getAutoUpdateValue("beam_break", 0)
 bb.addListener(valueChanged, NetworkTables.NotifyFlags.UPDATE)
 
 while True:
-    image_viewer.image_player("/home/pi/Downloads/trino2.jpg")
     time.sleep(1)
